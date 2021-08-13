4ee25e1762d1d9f125c6eae2158ec476f0395970
Daniil Ratakhin <ratahind@mail.ru>
2021-08-13 15:15:31 +0700

    initialCommit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,25 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "My Gatsby Site",
-  },
-  plugins: ["gatsby-plugin-styled-components"],
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "My Gatsby Site",
+    },
+    plugins: [
+        "gatsby-plugin-styled-components",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        {
+            resolve: 'gatsby-source-strapi',
+            options: {
+                apiURL: 'https://st.example.ipst-dev.com',
+                collectionTypes: [ // List of the Content Types you want to be able to request from Gatsby.
+                    'articles',
+                ],
+                singleTypes: [
+                    'info'
+                ],
+                queryLimit: 1000,
+            },
+        },
+    ],
+
 };
